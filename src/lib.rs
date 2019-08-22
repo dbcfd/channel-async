cf7ee998b36787b70cdfc5329bff826df53babf9
Danny Browning <danny.browning@protectwise.com>
2019-08-22 15:17:55 -0600

    cleanup cargo, add more tests

--- src/lib.rs
+++ src/lib.rs
@@ -26,10 +26,8 @@ mod tests {
     use super::*;
     use futures::StreamExt;
 
-    #[test]
-    fn send_receive() {
-        let rt = tokio::runtime::Runtime::new().expect("Failed to create runtime");
-
+    #[tokio::test]
+    async fn send_receive() {
         let (tx, rx) = unbounded(Duration::from_millis(100));
 
         let send_fut = async move {
@@ -43,9 +41,59 @@ mod tests {
             f
         };
 
-        rt.spawn(send_fut);
+        tokio::spawn(send_fut);
 
-        let recv = rt.block_on(recv_fut);
+        let recv = recv_fut.await;
+
+        assert_eq!(recv.len(), 100);
+    }
+
+    #[tokio::test()]
+    async fn send_receive_slow_sender_single_thread() {
+        let (tx, rx) = unbounded(Duration::from_millis(100));
+
+        let send_fut = async move {
+            for i in 0..100usize {
+                if i % 10 == 0 {
+                    tokio_timer::sleep(Duration::from_millis(500)).await;
+                }
+                tx.send(i).await.expect("Failed to send");
+            }
+        };
+
+        let recv_fut = async move {
+            let f: Vec<_> = rx.collect().await;
+            f
+        };
+
+        tokio::spawn(send_fut);
+
+        let recv = recv_fut.await;
+
+        assert_eq!(recv.len(), 100);
+    }
+
+    #[tokio::test(single_thread)]
+    async fn send_receive_slow_receiver() {
+        let (tx, rx) = unbounded(Duration::from_secs(1));
+
+        let send_fut = async move {
+            for i in 0..100usize {
+                if i % 10 == 0 {
+                    tokio_timer::sleep(Duration::from_millis(100)).await;
+                }
+                tx.send(i).await.expect("Failed to send");
+            }
+        };
+
+        let recv_fut = async move {
+            let f: Vec<_> = rx.collect().await;
+            f
+        };
+
+        tokio::spawn(send_fut);
+
+        let recv = recv_fut.await;
 
         assert_eq!(recv.len(), 100);
     }
